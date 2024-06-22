542d63f0bc327dcf8c3c908aa747dc9c90551847
Hajime Hoshi <hajimehoshi@gmail.com>
2024-06-22 15:06:08 +0900

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hajimehoshi/ebitenginegamejam2024
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 Hajime Hoshi
+
+package main
+
+func main() {
+}
