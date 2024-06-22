78fd41d9c27df5995f6c43c4d6c48a7afb6bd6df
Hajime Hoshi <hajimehoshi@gmail.com>
2024-06-22 15:19:54 +0900

    main: add ebiten.RunGame

--- main.go
+++ main.go
@@ -3,5 +3,30 @@
 
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 640, 480
+}
+
 func main() {
+	ebiten.SetWindowTitle("Ebitengine Game Jam 2024")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
 }
