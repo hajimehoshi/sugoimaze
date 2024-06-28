1b778a33985e896fa896f16e71fb4991001b2f9c
Hajime Hoshi <hajimehoshi@gmail.com>
2024-06-29 00:55:20 +0900

    all: add -trimpath

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cp $(go env GOROOT)/misc/wasm/wasm_exec.js ./html
-GOOS=js GOARCH=wasm go build -o=html/game.wasm .
+GOOS=js GOARCH=wasm go build -o=html/game.wasm -trimpath .
 zip -r html.zip ./html
